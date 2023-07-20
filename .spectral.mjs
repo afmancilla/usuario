0a43fbf1c314ad88de8825ffec63f4d2c581b443
Alex Mancilla <afmancilla@gmail.com>
2023-07-20 15:54:32 -0500

    Primera version del contrato

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxMTMxNA/spectral.js?branch_slug=master&token=e8abcc0e-e0c1-4b6c-919b-601c5abb3109";
+export default { extends: ruleset };
